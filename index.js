62ebab2a76a83b5f005b44b002cb50713dd318e0
Robert Castillo <roca1388@gmail.com>
2024-03-11 17:24:42 -0300

    update

--- index.js
+++ index.js
@@ -1,47 +1,24 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const mercadopago = require("mercadopago");
+const axios = require("axios");
 require("dotenv").config();
-
-mercadopago.configure({
-    access_token: process.env.ACCESS_TOKEN
-});
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("Todo okey");
 });
 
-app.post("/create_preference", cors(), (req, res) => {
-    let preference = {
-        items: req.body.items,
-        back_urls: {
-            success: "https://distribuidora-barbara.vercel.app/paymentsuccess",
-            failure: "https://distribuidora-barbara.vercel.app/checkout",
-            pending: "",
-        },
-        auto_return: "approved",
-        shipments: {
-            cost: req.body.shipment_cost,
-            mode: "not_specified",
-        },
-    };
-
-    mercadopago.preferences
-        .create(preference)
-        .then(function (response) {
-            res.header('Access-Control-Allow-Origin', 'https://distribuidora-barbara.vercel.app');
-            res.json({
-                id: response.body.id,
-            });
-        })
-        .catch(function (error) {
-            console.log(error);
-            res.status(500).send("Error interno del servidor");
-        });
+app.post("/create_preference", cors(), async (req, res) => {
+    try {
+        const response = await axios.post("https://backbarbara.vercel.app/create_preference", req.body);
+        res.json(response.data);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error interno del servidor");
+    }
 });
 
 const PORT = process.env.PORT || 8080; // Usar el puerto definido por el entorno o 8080 como predeterminado
